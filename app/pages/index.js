5b3a6e96888483686e5259f501ffb68c7284f7e7
Matthew Harward <matthew@cendenta.com>
2022-05-03 21:58:08 +0100

    CEN-21 Add app

---
+++ app/pages/index.js
@@ -0,0 +1,25 @@
+import Link from "next/link";
+import { supabase } from "../utils/supabase";
+
+export default function Home({ lessons }) {
+  console.log({ lessons });
+  return (
+    <div className="w-full max-w-3xl mx-auto my-16 px-2">
+      {lessons.map((lesson) => (
+        <Link key={lesson.id} href={`/${lesson.id}`}>
+            <a className="p-8 h-40 mb-4 rounded shadow text-xl flex">{lesson.title}</a>
+        </Link>
+      ))}
+    </div>
+  );
+}
+
+export const getStaticProps = async () => {
+  const { data: lessons } = await supabase.from("lesson").select("*");
+
+  return {
+    props: {
+      lessons,
+    },
+  };
+};
